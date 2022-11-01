0c555b9d07bda1fdf956514a005695e33b368cef
zeitderforschung <54367033+zeitderforschung@users.noreply.github.com>
2022-11-01 18:02:39 +0100

    improve robustness

--- elasticstore/lock.py
+++ elasticstore/lock.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import uuid
 
 from elasticsearch import AsyncElasticsearch
@@ -6,6 +7,17 @@ from elasticsearch import AsyncElasticsearch
 ACQUIRE_SCRIPT = """
     def now = System.currentTimeMillis();
     if (ctx.op == 'create' || now > ctx._source.lock_until) {
+        ctx._source.locked_by = params.owner;
+        ctx._source.locked_at = now;
+        ctx._source.lock_until = now + params.duration * 1000;
+    } else {
+        ctx.op = 'none';
+    }
+"""
+
+REFRESH_SCRIPT = """
+    def now = System.currentTimeMillis();
+    if (params.owner == ctx._source.locked_by) {
         ctx._source.locked_by = params.owner;
         ctx._source.locked_at = now;
         ctx._source.lock_until = now + params.duration * 1000;
@@ -22,6 +34,8 @@ RELEASE_SCRIPT = """
         ctx.op = 'none';
     }
 """
+
+DEFAULT_RETRY = 100
 
 
 class AlreadyLockedError(Exception):
@@ -45,14 +59,14 @@ class Lock():
         ----------
         es : AsyncElasticsearch
             The Elasticsearch client
-        index : str, optional
-            The Elasticsearch index name
         resource : str
             The locked resource
-        duration : int, optional
+        duration : int
             Period in seconds, which indicates how long the resource is blocked
-        refresh_interval : int, optional
+        refresh_interval : int
             Period in seconds after which the lock is refreshed
+        index : str
+            The Elasticsearch index name
         """
         self._es = es
         self._index = index
@@ -66,32 +80,57 @@ class Lock():
     def owner(self):
         return self._owner
 
-    async def _acquire(self):
-        script = dict(source=ACQUIRE_SCRIPT, params=dict(owner=self._owner, duration=self._duration))
-        req = dict(scripted_upsert=True, script=script, upsert={})
-        res = await self._es.update(index=self._index, id=self._resource, body=req, refresh=True)
+    async def _update(self, **kwargs):
+        return await self._es.update(
+            index=self._index,
+            id=self._resource,
+            body=kwargs,
+            retry_on_conflict=DEFAULT_RETRY,
+            refresh="wait_for")
+
+    async def _acquire_lock(self):
+        # to run the script whether or not the document exists,
+        # set scripted_upsert to true
+        res = await self._update(
+            scripted_upsert=True,
+            script=dict(
+                source=ACQUIRE_SCRIPT,
+                params=dict(owner=self._owner, duration=self._duration)),
+            upsert={})
         return res["result"] in ("created", "updated")
 
-    async def _release(self):
-        script = dict(source=RELEASE_SCRIPT, params=dict(owner=self.owner))
-        req = dict(script=script)
-        res = await self._es.update(index=self._index, id=self._resource, body=req, refresh=True)
+    async def _refresh_lock(self):
+        res = await self._update(
+            script=dict(
+                source=REFRESH_SCRIPT,
+                params=dict(owner=self._owner, duration=self._duration)))
         return res["result"] in ("updated")
 
-    async def _refresh(self):
+    async def _release_lock(self):
+        res = await self._update(
+            script=dict(
+                source=RELEASE_SCRIPT,
+                params=dict(owner=self.owner)))
+        return res["result"] in ("updated")
+
+    async def _refresh_loop(self):
         while True:
             await asyncio.sleep(self._refresh_interval)
-            if not await self._acquire():
+            if not await self._refresh_lock():
+                logging.warning(
+                    f'Lock for "{self._resource}" could not be refreshed')
                 break
 
     async def __aenter__(self):
-        if not await self._acquire():
-            raise AlreadyLockedError(f'Lock for "{self._resource}" could not be acquired by "{self._owner}"')
-        self._refresh_task = asyncio.create_task(self._refresh())
+        if not await self._acquire_lock():
+            raise AlreadyLockedError(
+                f'Lock for "{self._resource}" could not be acquired')
+        self._refresh_task = asyncio.create_task(self._refresh_loop())
         return self
 
     async def __aexit__(self, exc_type, exc, tb):
         assert self._refresh_task is not None
         self._refresh_task.cancel()
-        if not await self._release():
-            raise RuntimeError(f'Lock for "{self._resource}" could not be released by "{self._owner}"')
+        if not await self._release_lock():
+            logging.warning(
+                f'Lock for "{self._resource}" could not be released')
